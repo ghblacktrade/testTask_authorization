6097a519bee05890df47302d68697bb720a16c0a
Arseniy <97445616+ghblacktrade@users.noreply.github.com>
2023-06-13 15:26:02 +0300

    setMongo

--- server/jwtAuthorization/main.js
+++ server/jwtAuthorization/main.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
+const mongoose = require('mongoose')
 
-const PORT = 5001
+const PORT = process.env.PORT || 5001
 const app = express()
 
 app.listen(PORT, () => console.log(`Pasha server start on port ${PORT}`))
