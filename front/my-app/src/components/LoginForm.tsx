a5f240584ad1f89c6892cf20536c6845b9a69d21
Arseniy <97445616+ghblacktrade@users.noreply.github.com>
2023-06-13 21:59:26 +0300

    style_login

--- front/my-app/src/components/LoginForm.tsx
+++ front/my-app/src/components/LoginForm.tsx
@@ -1,4 +1,5 @@
 import React, {FC, useContext, useState} from 'react'
+import {type} from "os";
 
 const LoginForm: FC = () => {
 
@@ -6,35 +7,58 @@ const LoginForm: FC = () => {
     const [password, setIsPassword] = useState<string>()
 
     return (
-        <div>
+        <div className='fixed top-0 z-[100]'
+        >
+            <div
+                className='
+                    h-[100vh]
+                    w-[100vw]
+                    backdrop-blur-md
+                    fixed top-0'
 
-            <input
-                onChange={event => setIsEmail(event.target.value)}
-                value={email}
-                type='text'
-                placeholder='Enter your email'
-            />
-            <input
-                onChange={event => setIsEmail(event.target.value)}
-                value={password}
-                type='text'
-                placeholder='Enter your email'
-            />
-
-            <button
-
+                id='blur-div'
             >
-                Sign in
-            </button>
-            <button
 
+            </div>
+            <div className='
+                    h-[100vh]
+                    w-[100vw]
+                    flex
+                    flex-col
+                    justify-center
+                    items-center'
             >
-                Sign up
-            </button>
+                <div
+                    className='
+                        fixed
+                        z-[101]
+                        h-max
+                        w-max
+                        bg-white
+                        flex
+                        flex-col
+                        justify-center
+                        items-center'
 
+                    id='auth-modal'
+                >
+                    <div className='
+                            flex
+                            flex-col
+                            justify-center
+                            items-center
+                            p-8
+                            gap-7'
+                    >
+                        <h3 className='text-2xl font-semibold text-slate-700'>
+                            login
+                        </h3>
 
+                    </div>
+                </div>
+            </div>
         </div>
-    );
-};
+    )
+}
 
 export default LoginForm
